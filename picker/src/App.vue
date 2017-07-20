858b2581f69650508608fd3c423895b83ec67905
Ryan Collins <ryan@ohmgeek.co.uk>
2017-07-20 10:56:05 +0100

    Basic app header, and starting with more components

--- picker/src/App.vue
+++ picker/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    asdfasdfasdf
+    <div class="app-header">
+      UraDJ Jukebox System
+    </div>
+    <div class="page-container">
+    </div>
   </div>
 </template>
 
@@ -11,12 +15,29 @@ export default {
 </script>
 
 <style>
+html {
+  width: 100%;
+  top: 0;
+  display: block;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+}
+
+.app-header {
+  width: 100%;
+  background-color: blue;
+  top: 0px;
+  left: 0px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  color: white;
+  font-size: 18pt;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+}
+
+.page-container {
+  padding-top: 50px;
 }
 </style>
---
+++ picker/src/components/SearchBox.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="search-box">
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'search-box'
+}
+</script>
+
+
+<style scoped>
+
+</style>
