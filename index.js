75f2521c6813e740e05fc7c4f9002e8675346b6e
Ryan Collins <ryan@ohmgeek.co.uk>
2018-03-15 23:12:37 +0000

    Create the admin/manager for the jukebox queue

--- index.js
+++ index.js
@@ -20,91 +20,90 @@ app.set("port", process.env.PORT || 3000);
 
 // ------------- API calls ---------------
 function songAlreadyQueued(song) {
-  let backend = song.backend;
-  let id=song.info.id;
+    let backend = song.backend;
+    let id = song.info.id;
 
-  return new Promise((resolve, reject) => {
-    songs.forEach((s) => {
-      if(s.info.id == id && s.backend==backend) {
-        reject("Song Already In Queue");
-      }
-    });
-    resolve(song);
-  })
+    return new Promise((resolve, reject) => {
+        songs.forEach((s) => {
+            if (s.info.id == id && s.backend == backend) {
+                reject("Song Already In Queue");
+            }
+        });
+        resolve(song);
+    })
 }
 io.on('connection', function(conn) {
-  conn.on('search', (query) => {
-    var q = query.q;
-    YoutubeBackend.searchSongs(q).then(function(data) {
-      conn.emit('search-result', data);
+    conn.on('search', (query) => {
+        var q = query.q;
+        YoutubeBackend.searchSongs(q).then(function(data) {
+                conn.emit('search-result', data);
+            })
+            .catch((err) => {
+                conn.emit('search-result', { err: err });
+            });
     })
-    .catch((err) => {
-      conn.emit('search-result', {err: err});
-    });
-  })
 
-  conn.on('addSong', (songToAdd) => {
-    songAlreadyQueued(songToAdd).then((song) => {
-      songs.push(songToAdd);    
-      conn.emit('song-added', {confirmed: "Song added!"});
-      io.emit('queue-updated', songs);
-    }).catch((err) => {
-      conn.emit('song-added', {err: "Song already in queue"});
+    conn.on('addSong', (songToAdd) => {
+        songAlreadyQueued(songToAdd).then((song) => {
+            songs.push(songToAdd);
+            conn.emit('song-added', { confirmed: "Song added!" });
+            io.emit('queue-updated', songs);
+        }).catch((err) => {
+            conn.emit('song-added', { err: "Song already in queue" });
+        });
     });
-  });
 
-  conn.on('get-next-song', () => {
-    // We get the next song, broadcast it to everywhere else.
-    let song = songs.shift();
-    let backup = {
-      backend: 'youtube',
-      info: {
-        id: 'IwzUs1IMdyQ',
-        title: 'vitas',
-      }
-    }
-    if(song) {
-      io.emit('play-next-song', song);
-    } else {
-      io.emit('play-next-song', backup)
-    }
-  });
+    conn.on('get-next-song', () => {
+        // We get the next song, broadcast it to everywhere else.
+        let song = songs.shift();
+        let backup = {
+            backend: 'youtube',
+            info: {
+                id: 'IwzUs1IMdyQ',
+                title: 'vitas',
+            }
+        }
+        if (song) {
+            io.emit('play-next-song', song);
+        } else {
+            io.emit('play-next-song', backup)
+        }
+    });
 
 });
 
 
 app.get('/api/searchMusic', (req, res) => {
-  var q = req.query.q;
-  YoutubeBackend.searchSongs(q).then(function(data) {
-    res.send(data);
-  })
-  .catch((err) => {
-    res.send({ err: err });
-    console.log("error" + err);
-  });
+    var q = req.query.q;
+    YoutubeBackend.searchSongs(q).then(function(data) {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.send({ err: err });
+            console.log("error" + err);
+        });
 });
 
 app.post('/api/addSong', (req, res) => {
-  var songToAdd = req.body;
-  songs.push(songToAdd);
+    var songToAdd = req.body;
+    songs.push(songToAdd);
 
 
-  if(songToAdd in songs) {
-    res.send({
-      status: 200,
-      info: {
-        err: "Song already in the queue."
-      }
-    });
-  }
-  else {
-    res.send({
-      "status": 200,
-      "info": {
-        "message": "Song Added Successfully"
-      }
-    });
-  }
+    if (songToAdd in songs) {
+        res.send({
+            status: 200,
+            info: {
+                err: "Song already in the queue."
+            }
+        });
+    } else {
+        res.send({
+            "status": 200,
+            "info": {
+                "message": "Song Added Successfully"
+            }
+        });
+    }
 });
 
 app.get('/api/getQueue', (req, res) => {
@@ -117,7 +116,7 @@ app.get('/api/getQueue', (req, res) => {
 // ----------- STATIC CONTENT --------------
 app.use(express.static(path.join(currentDirectory, "picker")));
 app.use('/jukeboxplayer', express.static(path.join(currentDirectory, 'playback')));
-
+app.use('/jukeboxmanager', express.static(path.join(currentDirectory, 'admin')));
 app.get("*", function(req, res) {
     res.status(404).send("File not found");
 });
