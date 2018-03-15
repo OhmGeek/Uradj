812bc4841677a520c7454aca97bd8fe0ad06d24f
Ryan Collins <ryan@ohmgeek.co.uk>
2018-03-15 23:16:42 +0000

    limit result type and duration in search for YT

--- backends/YoutubeBackend.js
+++ backends/YoutubeBackend.js
@@ -2,45 +2,46 @@ var ytSearch = require('youtube-search');
 
 module.exports = {
     searchSongs: function(searchTerm) {
-      // this takes a search term,
-      // and returns a list of search results in
-      // our format
-          // set the youtube search options, including API key
-          console.log("YT SEARCH");
+        // this takes a search term,
+        // and returns a list of search results in
+        // our format
+        // set the youtube search options, including API key
+        console.log("YT SEARCH");
 
 
-          return new Promise(function(resolve, reject) {
+        return new Promise(function(resolve, reject) {
             const options = {
                 maxResults: 30,
-                key: process.env.YT_API_KEY
+                key: process.env.YT_API_KEY,
+                type: 'video',
+                videoDuration: ['short', 'medium']
             };
-            
+
             // search youtube for the input
-            ytSearch(searchTerm, options, function (err, results) {
+            ytSearch(searchTerm, options, function(err, results) {
                 console.log(results);
                 console.log(err)
                 if (err) return reject(err);
-                let data = (results.filter((x) => x.kind === "youtube#video"));
 
                 // Now go through and filter data into format.
                 let formattedResults = []
-                data.forEach(result => {
-                   console.log(result); 
-                   var track = {
-                    "backend": "youtube",
-                    "info": {
-                      "id": result.id,
-                      "name": result.title,
-                      "artist": "ARTIST",
-                      "length": "100",
-                      "thumbnail_img": result.thumbnails.default.url,
-        
+                results.forEach(result => {
+                    console.log(result);
+                    var track = {
+                        "backend": "youtube",
+                        "info": {
+                            "id": result.id,
+                            "name": result.title,
+                            "artist": "ARTIST",
+                            "length": "100",
+                            "thumbnail_img": result.thumbnails.default.url,
+
+                        }
                     }
-                  }
-                   formattedResults.push(track)
+                    formattedResults.push(track)
                 });
                 resolve(formattedResults);
             });
-          });
+        });
     },
-  }
+}
