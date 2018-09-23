8f2353b9f3f3e7f9066a5abf9cd596fb5ca7028a
Ryan Collins <ryan@ohmgeek.co.uk>
2018-09-23 20:56:13 +0100

    Fix display list on homepage

--- picker/src/components/HomePage.jsx
+++ picker/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ class HomePage extends React.Component {
                 </div>
                 <div className="w3-card-4">
                     <header className="w3-container w3-light-grey"><h3 className="w3-center">Up Next</h3></header>
-                    <article className="w3-container">
+                    <article>
                         <ShortQueue maxItemsToDisplay={5}/>
                     </article>
                 </div>
